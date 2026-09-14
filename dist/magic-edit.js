ab014b40de8b61563e5da16d2e5a349821f2c726
djson9 <json@middesk.com>
2026-09-14 19:56:35 -0400

    Fix unprivileged promotion version probe (#16)

--- dist/magic-edit.js
+++ dist/magic-edit.js
@@ -1,4 +1,4 @@
-/* @djson9/magic-edit v0.5.4 | https://github.com/djson9/magic-edit */
+/* @djson9/magic-edit v0.5.5 | https://github.com/djson9/magic-edit */
 (() => {
   if (document.getElementById('magic-edit-styles')) return
   const style = document.createElement('style')
